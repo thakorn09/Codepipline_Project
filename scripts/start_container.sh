a3da6e05658f17e1d863c81b216cb2bbbea41b05
Thanakorn Roopsom <57951881+thakorn09@users.noreply.github.com>
2020-05-19 01:07:43 +0700

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-docker pull 862605034458.dkr.ecr.ap-southeast-1.amazonaws.com/codepipeline:$IMAGE_TAG
-docker run -d -p 9000:9000 862605034458.dkr.ecr.ap-southeast-1.amazonaws.com/codepipeline:$IMAGE_TAG
-docker image
-docker ps
+echo start_container
