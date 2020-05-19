4e33c301c675ee4e12c38c4ab620f586618fdbc4
Thanakorn Roopsom <57951881+thakorn09@users.noreply.github.com>
2020-05-19 17:02:57 +0700

    Update before_startcon.sh

--- scripts/before_startcon.sh
+++ scripts/before_startcon.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 echo "Login to ECR"
-$sudo $(aws ecr get-login --no-include-email --region ap-southeast-1)
-COMMIT_HASH=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | cut -c 1-7)
-IMAGE_TAG=${COMMIT_HASH}
-echo $IMAGE_TAG
+
 
 
 
