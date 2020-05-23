df4a863c0922efd309df808bdf538c4facd0a638
thakorn09 <tanakon.rs@gmail.com>
2020-05-23 13:06:47 +0700

    fix file for deploy

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo 'start_container'
-docker pull 862605034458.dkr.ecr.ap-southeast-1.amazonaws.com/cicdproject:latest
-docker run -d -p 9000:9000 862605034458.dkr.ecr.ap-southeast-1.amazonaws.com/cicdproject:latest
+docker pull 862605034458.dkr.ecr.ap-southeast-1.amazonaws.com/codepipeline:latest
+docker run -d -p 9000:9000 862605034458.dkr.ecr.ap-southeast-1.amazonaws.com/codepipeline:latest
