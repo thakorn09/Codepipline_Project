40fac1c9eeb541dd9185e9b28ba0cb876c059cca
thakorn09 <tanakon.rs@gmail.com>
2020-06-02 02:31:49 +0700

    fix buildspec

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo 'start_container'
-docker pull 862605034458.dkr.ecr.ap-southeast-1.amazonaws.com/codepipeline:$IMAGE_TAG
-docker run -d -p 9000:9000 862605034458.dkr.ecr.ap-southeast-1.amazonaws.com/codepipeline:$IMAGE_TAG
+docker pull 862605034458.dkr.ecr.ap-southeast-1.amazonaws.com/codepipeline:latest
+docker run -d -p 9000:9000 862605034458.dkr.ecr.ap-southeast-1.amazonaws.com/codepipeline:latest
